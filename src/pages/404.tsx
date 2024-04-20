5f60e9b3e81c5215c688f896e23dd7c62fe1d861
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-20 22:25:32 +0800

    ✨ add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import PageHead from '@/components/PageHead'
+import Custom404Body from '@/sections/Custom404Body'
+
+export default function Custom404() {
+  return (
+    <>
+      <PageHead pageTitle="404" metaDescription="Page not found" />
+      <Custom404Body />
+    </>
+  )
+}
