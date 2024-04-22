0c841c522b59a325dc2af0eed39f2b0675d54bed
Cipto Hartanto <cipto.hartanto@yahoo.com>
2024-04-22 22:45:43 +0800

    ✨ [Update/v1.0.4] Add GA, enhance legibility of <ArticleBody /> and <CodeSnippet /> (#11)
    * 🐛 make text spacing legible on <ArticleBody />
    
    * 🐛 make <code /> and <pre /> more legible
    
    * 📦 add @next/third-parties
    
    * ✨ add GoogleTagManager to <MyApp />

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/index.scss'
 
+import { GoogleTagManager } from '@next/third-parties/google'
 import { ReactLenis } from '@studio-freight/react-lenis'
 import type { AppProps } from 'next/app'
 
@@ -7,6 +8,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ReactLenis root>
       <Component {...pageProps} />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_ID_GA ?? ''} />
     </ReactLenis>
   )
 }
