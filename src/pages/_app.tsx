fba411e451e55b19081ca2da051f0dc240012fed
Cipto Hartanto <cipto.hartanto@yahoo.com>
2026-06-28 21:14:46 +0800

    🔥 remove lenis from project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import '../styles/index.scss'
 
 import { GoogleTagManager } from '@next/third-parties/google'
-import { ReactLenis } from '@studio-freight/react-lenis'
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ReactLenis root>
+    <>
       <Component {...pageProps} />
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_ID_GA ?? ''} />
-    </ReactLenis>
+    </>
   )
 }
